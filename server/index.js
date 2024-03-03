736c68257af3869436572804f4ab008fda6d89a1
Oleg Akimov <akimba@gmail.com>
2024-03-03 07:17:28 +0300

    feat: Add database models

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { sequelize } from './db/db.js';
+import './models/models.js';
 
 const PORT = process.env.PORT ?? 5000;
 
