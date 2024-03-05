4491532ca84c2a228ed2a1d178b563acaa3473d6
Oleg Akimov <akimba@gmail.com>
2024-03-05 09:43:25 +0300

    db: Allows to create Type items in db & load them
    Работа с базой данных.
    Добавляет функционал добавления новых сущностей в базу данных
    И позволяет загружать их.
    Добавляемые сущности Type, отрабатывают в TypeController

--- server/controllers/type-controller.js
+++ server/controllers/type-controller.js
@@ -1,17 +1,23 @@
+import { Type } from './../models/models.js';
+import { ApiError } from './../error/api-error.js';
+
 class TypeController {
   async create(req, res, next) {
     try {
-
+      const { name } = req.body;
+      const type = await Type.create({ name });
+      return res.status(201).json(type);
     } catch (error) {
-
+      next(ApiError.badRequest(`db error happened: ${error.message}`));
     }
   }
 
-  async getAll(req, res, next) {
+  async getAll(_req, res, next) {
     try {
-
+      const types = await Type.findAll();
+      return res.status(200).json(types);
     } catch (error) {
-
+      next(ApiError.badRequest(`db error happened ${error.message}`));
     }
   }
 }
