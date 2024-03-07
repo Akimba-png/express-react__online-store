8d36b5dcd4e3168cfc243bcde98c26a98ca9c48b
Oleg Akimov <akimba@gmail.com>
2024-03-07 22:38:01 +0300

    server-db: Hash password & create user entity id db

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,14 +1,30 @@
 import { validationResult } from 'express-validator';
 import { ApiError } from '../error/api-error.js';
+import { User } from '../models/models.js';
+import bcrypt from 'bcrypt';
+
+const SALT = 5;
 
 class UserController {
   async registration(req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return next(ApiError.badRequest('form validation error', errors));
-    }
     try {
-
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.badRequest('form validation error', errors));
+      }
+      const { email, password } = req.body;
+      let candidate = await User.findOne({ where: { email } });
+      if (candidate) {
+        return next(
+          ApiError.badRequest(`user with email ${email} is already exists`)
+        );
+      }
+      const hashedPassword = await bcrypt.hash(password, SALT);
+      candidate = {
+        email,
+        password: hashedPassword,
+      };
+      const createdUser = await User.create(candidate);
     } catch (error) {
 
     }
